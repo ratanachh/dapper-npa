21ec69c6af00306305d8e3c648a10678214e67e5
Ratana <32198460+ratanachh@users.noreply.github.com>
2023-04-18 11:51:06 +0000

    test

--- src/DapperNpa.Example/Program.cs
+++ src/DapperNpa.Example/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class MyTest
+{
+    public int MyProperty { get; set; }
+}
+
+using Roslyn.Generated;
+
+[EqualityComparer2(typeof(MyTest))]
+public class Test 
+{
+
+}
