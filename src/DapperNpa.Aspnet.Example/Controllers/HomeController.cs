1a9ab2acebaad547e3534c2055f81131d17be0a1
Ratana CHHORM <chhormratana@gmail.com>
2023-04-25 09:34:42 +0700

    update

---
+++ src/DapperNpa.Aspnet.Example/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using DapperNpa.Aspnet.Example.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using DapperNpa.Aspnet.Example.Model;
+using Dapper;
+
+
+namespace DapperNpa.Aspnet.Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IDbConnection _connection;
+
+        public HomeController(IUserRepository userRepository, IDbConnection connection)
+        {
+            _userRepository = userRepository;
+            _connection = connection;
+        }
+        public User Index()
+        {
+            return _connection.Query<User>("SELECT 1 FROM user;").FirstOrDefault();
+        }
+    }
+}
